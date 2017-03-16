ca7743f5f49119029ef6df18657d8d5e527c4947
Ricky L <Ricky L>
2017-03-16 15:10:02 -0700

    fixed js, need to show lat/long and marker

--- hw9.js
+++ hw9.js
@@ -3,35 +3,103 @@ var map = new google.maps.Map(document.getElementById('map'), {
       center: new google.maps.LatLng(48.463649,  -123.311951),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-
-    var infowindow = new google.maps.InfoWindow();
-
-    var marker, i;
-    var count = 0;
-
-    for (i = 0; i < locations.length; i++) {  
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2], locations[i][3]),
-        map: map
-      });
-
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function(){
-          console.log (marker.position.lat(), marker.position.lng());
-          count++;
-          infowindow.setContent(locations[i][0]+"  "+marker.position.lat()+marker.position.lng()+"  count:"+count);
-          infowindow.open(map, marker);
+        function addMarker(location) {
+        marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
         }
-      })(marker, i));
+    //Test for browser compatibility
+if (window.openDatabase) {
+    //Create the database the parameters are 1. the database name 2.version number 3. a description 4. the size of the database (in bytes) 1024 x 1024 = 1MB
+    var mydb = openDatabase("locations_db", "0.1", "A Database of Favourite Locations", 1024 * 1024);
 
-      google.maps.event.addListener(map, 'zoom_changed', function() {
-          this.zoomChanged = true;
-      });
-    
-      google.maps.event.addListener(map, 'bounds_changed', function() {
-        if (this.zoomChanged) {
-            this.zoomChanged = false;
-          console.log (map.getBounds());
-        }
-      });
+    //create the cars table using SQL for the database using a transaction
+    mydb.transaction(function(t) {
+        t.executeSql("CREATE TABLE IF NOT EXISTS locations (id INTEGER PRIMARY KEY ASC, latitude TEXT, longitude TEXT)");
+    });
+
+
+
+} else {
+    alert("WebSQL is not supported by your browser!");
+}
+
+//function to output the list of cars in the database
+
+function updateLocList(transaction, results) {
+		console.log(transaction);
+    console.log(results);
+    //initialise the listitems variable
+    var listitems = "";
+    //get the car list holder ul
+    var listholder = document.getElementById("loclist");
+
+    //clear cars list ul
+    listholder.innerHTML = "";
+
+    var i;
+    //Iterate through the results
+    for (i = 0; i < results.rows.length; i++) {
+        //Get the current row
+        var row = results.rows.item(i);
+
+        listholder.innerHTML += "<li>" + row.lat + " - " + row.long + " (<a href='javascript:void(0);' onclick='deleteLoc(" + row.id + ");'>Delete Location</a>)";
     }
+
+}
+
+//function to get the list of cars from the database
+
+function outputLocs() {
+    //check to ensure the mydb object has been created
+    if (mydb) {
+        //Get all the cars from the database with a select statement, set outputCarList as the callback function for the executeSql command
+        mydb.transaction(function(t) {
+            t.executeSql("SELECT * FROM locations", [], updateLocList);
+        });
+    } else {
+        alert("db not found, your browser does not support web sql!");
+    }
+}
+
+//function to add the car to the database
+
+function addLoc() {
+    //check to ensure the mydb object has been created
+    if (mydb) {
+        //get the values of the make and model text inputs
+        var latitude = document.getElementById("latitude").value;
+        var longitude = document.getElementById("longitude").value;
+
+        //Test to ensure that the user has entered both a make and model
+        if (latitude !== "" && longitude !== "") {
+            //Insert the user entered details into the cars table, note the use of the ? placeholder, these will replaced by the data passed in as an array as the second parameter
+            mydb.transaction(function(t) {
+                t.executeSql("INSERT INTO locations (latitude, longitude) VALUES (?, ?)", [latitude, longitude]);
+                outputLocs();
+            });
+        } else {
+            alert("You must enter a latitude and longitude!");
+        }
+    } else {
+        alert("db not found, your browser does not support web sql!");
+    }
+}
+
+
+//function to remove a car from the database, passed the row id as it's only parameter
+
+function deleteLoc(id) {
+    //check to ensure the mydb object has been created
+    if (mydb) {
+        //Get all the cars from the database with a select statement, set outputCarList as the callback function for the executeSql command
+        mydb.transaction(function(t) {
+            t.executeSql("DELETE FROM locations WHERE id=?", [id], outputLocs);
+        });
+    } else {
+        alert("db not found, your browser does not support web sql!");
+    }
+}
+
+outputLocs();
